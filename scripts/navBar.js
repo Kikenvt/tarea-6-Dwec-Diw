1e700f1aab914a86f15f698f70c1fa0da34403a6
Kike <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2024-02-19 18:33:47 +0100

    Modificaciones en el index y los templates

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -38,3 +38,20 @@ window.addEventListener('scroll', function() {
         navMenu.classList.remove('active');
     }
 });
+
+
+// Scroll navigation
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.nav-item a');
+
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth"});
+        });
+    });
+}
