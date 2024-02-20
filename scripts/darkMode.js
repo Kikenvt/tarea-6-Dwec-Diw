ffc2c6ba7fa5ed3142338e2d6bcf400f4a4565cb
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2024-02-20 14:04:51 +0100

    mejorado dark mode

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,5 +1,7 @@
 function darkMode() {
     let element = document.body;
+    let services = document.querySelectorAll(".card");
     element.classList.toggle("light-theme");
+    services.classList.toggle("card-light-theme");
   }
 document.getElementById("darkMode").addEventListener("click", darkMode);
